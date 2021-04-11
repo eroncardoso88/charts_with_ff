5170c13501afc8379dc4d1e150749ce3d031e0d9
eron cardoso <eron.cardoso@conexasaude.com.br>
2021-04-11 14:34:53 -0300

    final solution

--- scripts/ScrollScripts.js
+++ scripts/ScrollScripts.js
@@ -2,68 +2,68 @@
 
     var whatToScroll = $('main')
     $("#butOne").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#one").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#one')[0].offsetTop - 150
         }, 2000);
     });
     $("#butTwo").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#two").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#two')[0].offsetTop - 150
         }, 2000);
     });
     $("#butThree").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#three").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#three')[0].offsetTop - 150
         }, 2000);
     });
     $("#butFour").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#four").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#four')[0].offsetTop - 150
         }, 2000);
     });
     $("#butFive").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#five").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#five')[0].offsetTop - 150
         }, 2000);
     });
     $("#butSix").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#six").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#six')[0].offsetTop - 150
         }, 2000);
     });
     $("#butSeven").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#seven").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#seven')[0].offsetTop - 150
         }, 2000);
     });
     $("#butEight").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#eight").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#eight')[0].offsetTop - 150
         }, 2000);
     });
     $("#butNine").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#nine").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#nine')[0].offsetTop - 150
         }, 2000);
     });
     $("#butTen").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#ten").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#ten')[0].offsetTop - 150
         }, 2000);
     });
     $("#butEleven").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#eleven").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#eleven')[0].offsetTop - 150
         }, 2000);
     });
     $("#butTwelve").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#twelve").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#twelve')[0].offsetTop - 150
         }, 2000);
     });
     $("#butThirteen").click(function() {
-        $(whatToScroll).animate({
-            scrollTop: $("#thirteen").offset().top
+        whatToScroll.animate({
+            scrollTop: $('#thirteen')[0].offsetTop - 150
         }, 2000);
     });
 })();
